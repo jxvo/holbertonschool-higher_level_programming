c6f5a7fbc1245254c84ccf198411d62e878e17bc
Jonathan Vasquez <jxvo92@yahoo.com>
2019-05-01 01:37:14 +0000

    Getting correct output, checker not happy

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-absolute = abs(number)
+absolute = number
 last = absolute % 10
 if number < 0:
-    last = -last
-print("Last digit of {:d} is {:d}".format(number, last), end = " ")
+    last = (abs(absolute) % 10) * -1
+print("Last digit of {} is {}".format(number, last), end = " ")
 if last > 5:
     print("and is greater than 5")
 elif last == 0:
