dfa6d2779374ff3afd8433318a275b94177d0681
Jonathan Vasquez <jxvo92@yahoo.com>
2019-05-24 02:17:55 +0000

    add two integers with error handling

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    def add_integer(a, b=98):
+        """ fad  """
+        if isinstance(a, float):
+            a = int(a)
+        if isinstance(b, float):
+            b = int(b)
+        if a is None or not isinstance(a, int):
+            raise TypeError("a must be an integer")
+        if b is None or not isinstance(b, int):
+            raise TypeError("b must be an integer")
+        return a + b
