de7b0ba479b1556ce030f22284938765ed2acfcc
Jonathan Vasquez <jxvo92@yahoo.com>
2019-06-06 21:55:08 +0000

    Modified if conditions to include negatives

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,6 +3,6 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, "r") as txt:
         count = 0
         for line in txt:
-            if nb_lines == 0 or count < nb_lines:
+            if nb_lines <= 0 or count < nb_lines:
                 print(line, end="")
                 count += 1
