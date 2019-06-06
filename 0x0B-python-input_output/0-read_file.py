beb1335ef7eb565e86b812169e4387c98ef3b852
Jonathan Vasquez <jxvo92@yahoo.com>
2019-06-06 21:14:06 +0000

    looping through each line instead of just read()

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, "r") as txt:
-        print(txt.read())
+        for line in txt:
+            print(line, end="")
     txt.closed
