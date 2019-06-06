bee2b281fdba8b3bd8274ae761bee6ad3e2b38d3
Jonathan Vasquez <jxvo92@yahoo.com>
2019-06-06 21:44:39 +0000

    read n lines of text from a UTF8 text file and print to stdout

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    with open(filename, "r") as txt:
+        count = 0
+        for line in txt:
+            if nb_lines == 0 or (count <= nb_lines and nb_lines != 0):
+                print(line, end="")
+                count += 1
