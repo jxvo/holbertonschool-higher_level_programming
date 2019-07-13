96d45eb869a21f033485f33244622532e649c50b
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-13 02:14:19 +0000

    write the JSON representation of a list of objects to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,16 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON representation of a list of objects to a file"""
+        if list_objs is None:
+            with open(cls.__name__ + ".json", "w+") as json_file:
+                json_file.write(cls.to_json_string(None))
+        else:
+            dict_list = []
+            for obj in list_objs:
+                dict_list.append(obj.to_dictionary())
+            with open(cls.__name__ + ".json", "w+") as json_file:
+                json_file.write(cls.to_json_string(dict_list))
