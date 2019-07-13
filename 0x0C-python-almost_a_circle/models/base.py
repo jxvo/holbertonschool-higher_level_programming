1266eae660968f866bd83eebdf6ff143c07f2ed0
Jonathan Vasquez <jxvo92@yahoo.com>
2019-07-13 02:01:25 +0000

    define to_json_string which returns ouputs JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module for Base model"""
-
+import json
 
 class Base:
     """Base class contains methods and id for all data models"""
@@ -16,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON representation of list of dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
