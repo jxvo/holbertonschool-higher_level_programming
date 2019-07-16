0dd9bc4c972d2be70f8a5e760216005abd7d5b7f
Jonathan Vasquez <727@holberton.com>
2019-07-16 23:53:31 +0000

    pushing foundation for advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
+
+/*
+ * print_python_list - using CPython, print information about the contents
+ * @p: pointer to python list PyObject
+ *
+ * Return: nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *ssssss;
+}
