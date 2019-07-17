701e0a2447d4f2a7385adccb0062c1d849cbb384
Jonathan Vasquez <727@holberton.com>
2019-07-17 06:42:27 +0000

    defined sqaure

---
+++ 0x06-python-classes/101-sqaure.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+class Square:
+    """contains methods for our shape"""
+    def __init__(self, size=0, position=(0, 0)):
+        """initialize an instance of a square"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """returns private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """validates and sets private attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size mmust be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) < 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__position[1]):
+                print()
+            for row in range(0, self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
