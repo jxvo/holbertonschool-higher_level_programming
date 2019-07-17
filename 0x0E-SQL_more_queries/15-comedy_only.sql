b8067dbf87d46ea9865b341b86a0c72ea8315cf8
Jonathan Vasquez <727@holberton.com>
2019-07-17 04:04:35 +0000

    fixed wrong comment syntax

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
-- list all comedy shows
+-- list all comedy shows
 SELECT tv_shows.title FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
