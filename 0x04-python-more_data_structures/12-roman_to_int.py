60ea817045bd80259cb6634400497d34d5c4f054
Jonathan Vasquez <727@holberton.com>
2019-07-17 04:38:14 +0000

    PEP8 shortened return integer variable name:

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,13 +6,13 @@ def roman_to_int(roman_string):
         'C': 100, 'D': 500,
         'M': 1000
     }
-    to_int = 0
+    num = 0
 
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     for idx in range(len(roman_string)):
         if idx > 0 and roman[roman_string[idx]] > roman[roman_string[idx - 1]]:
-            to_int += roman[roman_string[idx]] - 2 * roman[roman_string[idx - 1]]
+            num += roman[roman_string[idx]] - 2 * roman[roman_string[idx - 1]]
         else:
-            to_int += roman[roman_string[idx]]
-    return to_int
+            num += roman[roman_string[idx]]
+    return num
