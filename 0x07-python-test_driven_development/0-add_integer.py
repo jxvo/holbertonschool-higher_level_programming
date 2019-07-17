bd508973256fdcb118a52644e07e88b182a7b5da
Jonathan Vasquez <727@holberton.com>
2019-07-17 08:33:51 +0000

    adds two integers with error handling and float to int conversion

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def add_integer(a, b=98):
+def add_integer(a, b=98):
         """ fad  """
         if isinstance(a, float):
             a = int(a)
         if isinstance(b, float):
             b = int(b)
-        if a is None or not isinstance(a, int):
+        if not isinstance(a, (int, float)):
             raise TypeError("a must be an integer")
-        if b is None or not isinstance(b, int):
+        if not isinstance(b, (int, float)):
             raise TypeError("b must be an integer")
         return a + b
