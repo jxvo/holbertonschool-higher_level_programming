0b0e02e60606ff955d5a312ada9c61f3237f1e80
Jonathan Vasquez <727@holberton.com>
2019-07-17 06:11:39 +0000

    modified assignment of num to 0 when exception occurs

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,13 +5,14 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             div = my_list_1[idx] / my_list_2[idx]
         except ZeroDivisionError:
+            div = 0
             print("division by 0")
         except TypeError:
+            div = 0
             print("wrong type")
         except IndexError:
-            print("out of range")
-        except:
             div = 0
+            print("out of range")
         finally:
             list_results.append(div)
     return list_results
