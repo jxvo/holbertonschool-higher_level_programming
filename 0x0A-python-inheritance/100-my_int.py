75a9f02cbaf370cac12b309b67dcf4c88b0fa0e2
Jonathan Vasquez <727@holberton.com>
2019-07-20 10:02:07 +0000

    fixed output

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,9 +4,12 @@
 
 class MyInt(int):
     """MyInt is a rebel that has == and != operators inverted"""
+    def __init__(self, value):
+        self.value = value
+
     def __eq__(self, other):
         """when obj is compared as == it should be interpreted as !="""
-        return super() != other
+        return self.value != other
 
     def __ne__(self, other):
-        return super() == other
+        return self.value == other
