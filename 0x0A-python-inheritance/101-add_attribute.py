c97b333de62b8770f8b43b22a5efb61ddec363b2
Jonathan Vasquez <727@holberton.com>
2019-07-20 13:59:01 +0000

    changed conditional on line 4 to AND

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Advanced Task 13"""
 def add_attribute(obj, attr, value):
-    if hasattr(obj, "__dict__") or  hasattr(obj, "__slots__"):
+    if hasattr(obj, "__dict__") and hasattr(obj, "__slots__"):
         setattr(obj, attr, value)
     else:
         raise TypeError("can't add new attribute")
