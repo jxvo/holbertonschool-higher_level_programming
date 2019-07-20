7e2cf0c6656c821479e1e7c47c6c8e5d7643e69f
Jonathan Vasquez <727@holberton.com>
2019-07-20 14:00:18 +0000

    changed conditional on line 4 to AND + PEP8 double newline fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Advanced Task 13"""
+
+
 def add_attribute(obj, attr, value):
     if hasattr(obj, "__dict__") and hasattr(obj, "__slots__"):
         setattr(obj, attr, value)
