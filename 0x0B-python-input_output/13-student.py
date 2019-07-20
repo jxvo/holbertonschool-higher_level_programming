3c80ea7f095dad5c72b2e3a74b332e4249dc0fa5
Jonathan Vasquez <727@holberton.com>
2019-07-20 14:44:02 +0000

    implemented reload from json

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Student():
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None and type(attrs) is not list:
+            return self.__dict__
+        for attribute in attrs:
+            if type(attribute) is not str:
+                return self.__dict__
+        obj = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                obj.update({key: value})
+        return obj
+
+    def reload_from_json(self, json):
+        for key, val in json.items():
+            setattr(self, key, val)
