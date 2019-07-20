27d17c3a7e5ff873d81b4acdcea3075fd3d7b4fa
Jonathan Vasquez <727@holberton.com>
2019-07-20 22:52:43 +0000

    handling appending at end of list when search is found

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,10 @@
 def append_after(filename="", search_string="", new_string=""):
     """opens a file, parses each line to match string, and inserts new string"""
     with open(filename, "r+") as txt_file:
-        lines = txt_file.readlines()
-        for idx in range(len(lines)):
-            if search_string in lines[idx]:
-                lines.insert(idx + 1, new_string)
+        lines = []
+        for line in txt_file:
+            lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
         with open(filename, "w+") as txt_file:
             txt_file.write("".join(lines))
