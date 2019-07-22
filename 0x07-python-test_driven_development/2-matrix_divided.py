170dca36c6b8cbf55ac2564fa049c9b62be323a4
Jonathan Vasquez <727@holberton.com>
2019-07-22 19:10:48 +0000

    convered long error to variable to prevent long lines

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     """all elements of matrix (list of lists) are divided by div"""
+
+    __long_error = "matrix must be a matrix (list of lists) of integers/floats"
+
     if len(matrix) < 1:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(long_error)
     if not isinstance(div, (float, int)):
         raise TypeError("div must be an integer")
     if div == 0:
@@ -13,8 +15,7 @@ def matrix_divided(matrix, div):
 
     for row in matrix:
         if len(row) < 1:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(long_error)
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
 
@@ -22,9 +23,7 @@ def matrix_divided(matrix, div):
 
         for val in row:
             if not isinstance(val, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of\
-                    lists) of integers/floats")
+                raise TypeError(long_error)
             new_row.append(round(val / div, 2))
         new_matrix.append(new_row)
     return new_matrix
