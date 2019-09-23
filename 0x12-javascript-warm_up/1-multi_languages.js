b437c475594a766b3d31dedae0b51cf30981df4d
jxvo <16144089+jxvo@users.noreply.github.com>
2019-09-23 22:27:22 +0000

    print 3 lines to stdout (my weapon of choice is the for loop)

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* prints 3 lines (using for loop) */
+const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (var idx = 0; idx < 3; idx++) {
+  console.log(myArr[idx]);
+}
