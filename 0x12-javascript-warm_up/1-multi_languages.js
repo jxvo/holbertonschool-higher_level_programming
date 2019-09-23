a64711f336332973277bb437cc841df0b546afd6
jxvo <16144089+jxvo@users.noreply.github.com>
2019-09-23 22:31:29 +0000

    requirements prohibit use of 'var' so brute force method used instead

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 /* prints 3 lines (using for loop) */
-const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (var idx = 0; idx < 3; idx++) {
-  console.log(myArr[idx]);
-}
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is awesome');
