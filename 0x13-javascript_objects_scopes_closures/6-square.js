7ef866fdaf6a45d18f930461d132e4c98749b21a
jxvo <16144089+jxvo@users.noreply.github.com>
2019-09-24 00:32:26 +0000

    defined charPrint() method with fallback super.print()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,43 +1,17 @@
 #!/usr/bin/node
-// Rectangle class
-class Rectangle {
-  // creates empty object if invalid params passed
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  // print a visual representation to stdout
-  print () {
-    const symbol = 'X';
-    for (let line = 0; line < this.height; line++) {
-      console.log(symbol.repeat(this.width));
-    }
-  }
-
-  // switch height and width values
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  // double the width and height values
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
 // Square class
-class Square extends Rectangle {
-  // inherits size value from Rectangle
-  constructor (size) {
-    super(size, size);
+class Square extends require('./5-square.js') {
+  // prints a visual representation made from 'c'
+  charPrint(c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      const symbol = c;
+      for (let line = 0; line < this.height; line++) {
+	console.log(symbol.repeat(this.width));
+      }
+    }
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
