272ede7b2fab553ced94bc2b59aaa1166a0330db
jxvo <16144089+jxvo@users.noreply.github.com>
2019-09-24 00:33:19 +0000

    semistandard fixes bruh

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,13 @@
 // Square class
 class Square extends require('./5-square.js') {
   // prints a visual representation made from 'c'
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       super.print();
     } else {
       const symbol = c;
       for (let line = 0; line < this.height; line++) {
-	console.log(symbol.repeat(this.width));
+        console.log(symbol.repeat(this.width));
       }
     }
   }
