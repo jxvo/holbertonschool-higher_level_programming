0ac8ca13c27cd6ade1b86ec227e8dddf7ea62697
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-10-09 03:52:22 +0000

    attempting case sensitive query using BINARY

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' \
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' \
     ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
 
