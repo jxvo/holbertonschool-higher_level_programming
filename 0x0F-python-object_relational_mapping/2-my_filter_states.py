34176909166f63023ef1406a04ffeaf2cb8e465f
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-10-09 03:48:02 +0000

    switched to use of .format as required by checker

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s \
-    ORDER BY id ASC", [sys.argv[4]])
+    cur.execute("SELECT * FROM states WHERE name = '{}' \
+    ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
