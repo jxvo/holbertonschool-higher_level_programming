7eecf9586536403a6d7985b1f91fa417e22564ec
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-10-09 06:17:51 +0000

    pep8 line too long -___-"

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
 
     cur.execute(
         "SELECT cities.id, cities.name, states.name FROM cities \
-        INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC"
+        INNER JOIN states ON states.id = cities.state_id \
+        ORDER BY cities.id ASC"
     )
     rows = cur.fetchall()
 
