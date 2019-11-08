5ba67a20b7feb9fe2d8c1fc29b2babdd966e0eef
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 07:03:34 +0000

    display formatted error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""returns the http request or error code"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req._content.decode())
