e6575142c47a3be35ae162f0880047df099bdcb4
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 01:29:13 +0000

    list all state objects in a database using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module to list all 'state' objects in a database using sqlalchemy
+args: [mysql username] [mysql password] [database name]
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = ccreate_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id.asc()):
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
