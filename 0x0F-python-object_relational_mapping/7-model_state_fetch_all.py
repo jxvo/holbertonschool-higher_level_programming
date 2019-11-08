00e8b1cdf6235bfe1b942af64c7d2c00d8022159
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 01:32:55 +0000

    pep8 newline at end of file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,9 +10,9 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-    engine = ccreate_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
