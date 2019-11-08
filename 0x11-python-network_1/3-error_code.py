aeb80bc4cbf11f5f4e04c2ec071ab87ed89484af
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 06:49:19 +0000

    send POST request and print formatted error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Send request to URL http://0.0.0.0:5000 within container"""
+from sys import argv
+import urllib.request
+
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            content = response.read()
+            print(content.decode('utf-8'))
+    except urllib.error.HTTPError as errcode:
+        print("Error code: {}".format(errcode.code))
