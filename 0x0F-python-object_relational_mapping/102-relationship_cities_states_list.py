1231b6cd59e04df0cbb854f414ed15171e11287b
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 03:06:14 +0000

    query only cities values

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+query only 'cities' values from 'states' using sqlalchemy
+usage: [mysql username] [mysql password] [database name]
+"""
+
+
+from sys import argv
+from relationship_city import Base, City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City)
+
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    session.close()
