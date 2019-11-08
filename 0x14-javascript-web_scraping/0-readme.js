a6be6464b39938389f34b812911a896d5dd60184
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 17:57:04 +0000

    read the contents of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const source = process.argv[2];
+
+fs.readFile(source, 'utf8', (err, source) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(source);
+});
