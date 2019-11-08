0edb7741dae968f9c6fa6bf3acf5c4e16b7de009
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 16:02:41 +0000

    convert parameter to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const process = require('process');
 if (isNaN(process.argv[2])) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
