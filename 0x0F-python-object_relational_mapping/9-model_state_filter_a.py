75ad86699fdd8617473b0765d203d382c886e5ca
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 02:08:23 +0000

    documentation was placed at wrong level oops

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,22 +5,16 @@ usage: [mysql username] [mysql password] [database name]
 """
 import sys
 from model_state import Base, State
-from sqlalchemy
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-
 
 if __name__ == "__main__":
     engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).\
-        order_by(State.id.asc())
-
-    for state in states:
+    for state in session.query(State).filter(
+            State.name.contains("a")).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
-
     session.close()
