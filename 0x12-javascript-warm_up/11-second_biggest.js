39eebd99b32bf4084ba8cce895b57977cb154cda
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 16:13:24 +0000

    search for second biggest integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,16 @@
 #!/usr/bin/node
 
-console.log(process.argv.slice(2).length);
-console.log(process.argv.length);
+const len = process.argv.length;
+if (process.argv[2] === undefined) {
+  console.log(0);
+} else if (len === 3) {
+  console.log(0);
+} else {
+  const arr = [];
+  for (let index = len; index > 2; index--) {
+    arr.push(parseInt(process.argv[index - 1]));
+  }
+  arr.sort((left, right) => left - right);
+  const ret = len - 4;
+  console.log(arr[ret]);
+}
