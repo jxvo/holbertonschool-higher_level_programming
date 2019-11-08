71b31cb37ffe14b7a2540765e3beed12b4a40a44
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 02:10:00 +0000

    made line 12 shorter by using variable

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,11 +5,11 @@ usage: [mysql username] [mysql password] [database name]
 """
 import sys
 from model_state import Base, State
-import sqlalchemy
+import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
