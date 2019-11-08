be7f59a57e005b93c2b1e539c6164c0d1602af5f
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 06:43:18 +0000

    display x-request-id from header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Display X-Request-Id variable from HTTP header"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header.get('X-Request-Id'))
+        
