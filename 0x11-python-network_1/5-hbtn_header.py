970e6f5eafbd4ea063c2b3763161995542cca740
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 07:00:43 +0000

    display x-request-id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,7 +3,7 @@
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    req = requests.get(argv[1])
-    print(req.headers['x-request-id'])
+    respo = requests.get(argv[1])
+    set = respo.headers.get('X-Request-Id')
+    print(set)
