5f34812d2a9fbb6b7c607ce48682ce4293fdf07c
Jonathan Vasquez <16144089+jxvo@users.noreply.github.com>
2019-11-08 03:03:15 +0000

    list all states and their cities values using sqlalchemy

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,21 +3,28 @@
 list all 'state' and corresponding 'cities' values using sqlalchemy
 usage: [mysql username] [mysql password] [database name]
 """
-import sys
+from sys import argv
 from relationship_city import Base, City
 from relationship_state import State
-import sqlalchemy as db
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import joinedload
 
 
-if __name__ == "__main__":
-    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).all()
-    for i in state:
-        print(i.child)
+    state = session.query(State).options(joinedload(State.cities))
+
+    for states in state:
+        print("{}: {}".format(states.id, states.name))
+        for i in range(len(states.cities)):
+            print("\t{}: {}".format(
+                states.cities[i].id, states.cities[i].name))
+
     session.close()
